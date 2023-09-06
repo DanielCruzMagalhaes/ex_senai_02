3b3f15fe4ccaeca620806c933945d03353a222b7
DanielCruzMagalhaes <danielcruzmagalhaes@gmail.com>
2023-09-05 21:41:36 -0300

    Adicionados os estilos na pasta static

--- projeto/setup/settings.py
+++ projeto/setup/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for setup project.
 
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app',
+    'app"'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'app/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
-
+STATICFILES_DIRES = [
+    os.path.join(BASE_DIR,'app/static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
